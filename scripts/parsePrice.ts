f91542bb36a7063c1e3b5f5f633ff69b8687f2c7
Chang Wang <garnwraly@gmail.com>
2020-04-06 22:06:13 -0400

    add util fn to parse prices

---
+++ scripts/parsePrice.ts
@@ -0,0 +1,11 @@
+import { Price } from '../src/types';
+
+export const parsePrice = (priceString: string): Price | null => {
+  const matched = priceString.replace(/,/g, '').match(/([\d]+)x*\s(.+)/);
+  return matched
+    ? {
+        amount: Number(matched[1]),
+        currency: matched[2],
+      }
+    : null;
+};
