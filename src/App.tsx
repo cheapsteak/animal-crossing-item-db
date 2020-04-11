fe59043bdc71c892f0b54c13bb35be0f6b561dd8
Chang Wang <garnwraly@gmail.com>
2020-04-10 22:57:25 -0400

    render all items in virtualized list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,50 @@
 import React, { Suspense, useState, useMemo } from 'react';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
+
 import { useItemsData } from './useItemsData';
 import { Furniture, Fish, Bug } from './types';
+
+const numberFormatter = new Intl.NumberFormat();
+
+const ItemRenderer: React.FC<ListChildComponentProps> = ({
+  data,
+  index,
+  style,
+}) => {
+  const x = data[index];
+  return (
+    <div
+      key={x.type + x.name}
+      style={{ ...style, display: 'flex', alignItems: 'center' }}
+    >
+      <span>{x.name}</span>
+      {x.imageName && (
+        <img src={`${x.imageName}`} style={{ width: 32 }} alt="" />
+      )}
+      <div style={{ marginLeft: 'auto' }}>
+        {x.type !== 'furniture' ? (
+          <span>{numberFormatter.format(x.price)}</span>
+        ) : (
+          <span>
+            {x.price.sell?.amount && (
+              <>
+                sell: {numberFormatter.format(x.price.sell?.amount)}{' '}
+                {x.price.sell?.currency};
+              </>
+            )}{' '}
+            {x.price.buy?.amount && (
+              <>
+                buy: {numberFormatter.format(x.price.buy?.amount)}{' '}
+                {x.price.buy?.currency}
+              </>
+            )}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Foo = () => {
   const { furniture, fish, bugs } = useItemsData({ hemisphere: 'northern' });
@@ -27,42 +71,27 @@ const Foo = () => {
     return allItems.filter((item) => queryRegex.test(item.name));
   }, [queryRegex, furniture, fish, bugs]);
 
-  const numberFormatter = useMemo(() => new Intl.NumberFormat(), []);
-
   return (
-    <div>
+    <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
       <input
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <div>
-        {searchResults.slice(0, 50).map((x) => (
-          <div key={x.type + x.name}>
-            <span>{x.name}</span>
-            {x.imageName && (
-              <img src={`${x.imageName}`} style={{ width: 32 }} alt="" />
-            )}
-            {x.type !== 'furniture' ? (
-              <span>{numberFormatter.format(x.price)}</span>
-            ) : (
-              <span>
-                {x.price.sell?.amount && (
-                  <>
-                    sell: {numberFormatter.format(x.price.sell?.amount)}{' '}
-                    {x.price.sell?.currency};
-                  </>
-                )}{' '}
-                {x.price.buy?.amount && (
-                  <>
-                    buy: {numberFormatter.format(x.price.buy?.amount)}{' '}
-                    {x.price.buy?.currency}
-                  </>
-                )}
-              </span>
-            )}
-          </div>
-        ))}
+      <div style={{ flexGrow: 1 }}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <FixedSizeList
+              itemData={searchResults}
+              itemCount={searchResults.length}
+              height={height}
+              width={width}
+              itemSize={40}
+            >
+              {ItemRenderer}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
       </div>
     </div>
   );
